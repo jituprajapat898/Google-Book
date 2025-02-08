bb96b3429380fc27acff5040f46a84f8e1c69ad6
jituprajapat898 <jituprajapat898@gmail.com>
2025-02-08 22:38:20 +0530

    Fix:Google Book

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Main from './Components/Main';
+import './Components/style.css'
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
